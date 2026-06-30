c520c6d839c7d3706183857fff3db93e01810e2e
Andrey Ch <63464663+Ader01@users.noreply.github.com>
2026-07-01 01:45:52 +0300

    коррекция имени проекта

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def main():
     print("Hello from test-lib-proj!")
 
+def test_connection() -> str:
+    """Функция для проверки успешного подключения библиотеки."""
+    return "Библиотека test-Lib корректно подключена через uv."
+
 
 if __name__ == "__main__":
     main()
